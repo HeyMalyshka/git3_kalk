a43e7e4f2647c1afd186622520503f67449b33bd
Tagreeg <vovosel2413@gmail.com>
2022-03-26 12:38:51 +0500

    sqrt

--- git3_kalk/Program.cs
+++ git3_kalk/Program.cs
@@ -6,17 +6,8 @@ namespace git3_kalk
     {
         static void Main(string[] args)
         {
-            int a;
-            int b;
-            int c;
-            int z;
-
-            Console.WriteLine("Ведите а");
-            a =Convert.ToInt32(  Console.ReadLine());
-            Console.WriteLine("Ведите b");
-            b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Ведите знак \n 1)+ \n 2)- \n 3)* \n 4)/ \n 5)фактариал 6)корень");
-            z = Convert.ToInt32(Console.ReadLine());
+            else if (z == 6)
+                Console.WriteLine("koren a = " + Math.Sqrt(a));
         }
     }
 }
