8caddfc5709b7e267400f2da4657fbf102b117e0
Your Name <you@example.com>
2022-03-26 10:48:41 +0500

    Добавьте файлы проекта.

---
+++ git3_kalk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git3_kalk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
