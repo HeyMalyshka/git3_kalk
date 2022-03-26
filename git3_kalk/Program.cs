21db517fa6e1bd7f83202afe4f0b88f1c00cda10
Tagreeg <vovosel2413@gmail.com>
2022-03-26 14:25:51 +0500

    pft,fkcz

--- git3_kalk/Program.cs
+++ git3_kalk/Program.cs
@@ -6,12 +6,37 @@ namespace git3_kalk
     {
         static void Main(string[] args)
         {
+
             else if (z == 5)
                 Console.WriteLine("a! = ");
             int factorial = 1;
             for (int i = 1; i <= a; i++)
                 factorial *= i;
             Console.Write(factorial);
+
+
+
+
+            if (z == 1)
+            {
+                Console.WriteLine(a + b);
+            }
+
+            else if (z == 2)
+            {
+                Console.WriteLine(a - b);
+            }
+
+            else if (z == 6)
+                Console.WriteLine("koren a = " + Math.Sqrt(a));
+
+
+            else if (z == 3)
+                Console.WriteLine("a*b = " + a*b);
+            
+            else if (z == 4)
+                Console.WriteLine("a/b = " + a / b);
+
 
         }
     }
